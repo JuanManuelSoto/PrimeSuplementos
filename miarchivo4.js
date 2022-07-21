49b5f014b5485abcc41fad8a7e028cffb2899e9b
JuanManuelSoto <juanma.soto.tonini@hotmail.com>
2022-07-20 23:20:44 -0300

    clase15Soto

--- miarchivo4.js
+++ miarchivo4.js
@@ -1,3 +1,18 @@
+const API_URL = 'https://api.mercadolibre.com/';
+
+const endpointProductos = '/sites/MLA/search';
+
+let productos;
+
+const traerDatos = () => {
+    fetch(API_URL + endpointProductos+ '?q=proteínawhey')
+    .then((response) => response.json())
+    .then((data) => {
+        productos = data.results;
+        generarCards(productos);
+    }) 
+}
+
 document.getElementById ("tituloDelSitio").innerHTML = "Prime Suplementos"
 
 document.getElementById ("subtituloDelSitio").innerHTML = "Potencia tus resultados al máximo"
@@ -62,57 +77,58 @@ function validarEmail(email) {
     
     const carrito = JSON.parse (carritoStorage) ?? [];
     
-    const arrayDeProductos = [{
-        id: 1,
-        nombre: "Whey Protein",
-        precio: 4500,
-        stock: "hay stock"
-    },
-    {
-        id: 2,
-        nombre: "Creatina",
-        precio: 6500,
-    stock: "sin stock",
-},
-{
-    id: 3, 
-    nombre: "Pre-workout",
-    precio: 3000,
-    stock: "sin stock"
-},
-{
-    id: 4,
-    nombre: "Aminoacidos BCAA",
-    precio: 1500,
-    stock: "sin stock"
-}
-];
+//     const arrayDeProductos = [{
+//         id: 1,
+//         nombre: "Whey Protein",
+//         precio: 4500,
+//         stock: "hay stock"
+//     },
+//     {
+//         id: 2,
+//         nombre: "Creatina",
+//         precio: 6500,
+//         stock: "sin stock",
+//     },
+//     {
+//         id: 3, 
+//         nombre: "Pre-workout",
+//         precio: 3000,
+//         stock: "sin stock"
+//     },
+//     {
+//         id: 4,
+//         nombre: "Aminoacidos BCAA",
+//         precio: 1500,
+//         stock: "sin stock"
+//     }
+// ];
 
+const generarCards = (arrayDe) => {
 let acumulador = ``;
-arrayDeProductos.forEach((producto) => {
+arrayDe.forEach((producto) => {
     acumulador += `<div class="col mb-5">
     <div class="card h-100">
     <!-- Product image-->
-    <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+    <img class="card-img-top" src="${producto.thumbnail}" alt="..." />
     <!-- Product details-->
     <div class="card-body p-4">
     <div class="text-center">
     <!-- Product name-->
-    <h5 class="fw-bolder">${producto.nombre}</h5>
+    <h5 class="fw-bolder">${producto.title}</h5>
     <!-- Product price-->
-    $${producto.precio}
+    $${producto.price}
     </div>
     </div>
     <!-- Product actions-->
     <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
     <div class="text-center"><a onclick = "agregarAlCarrito(${producto.id})" 
-    class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+    class="btn btn-outline-dark mt-auto" href="#">Agregar al carrito</a></div>
     </div>
     </div>
     </div>`;
-});
+    document.getElementById ("card-container").innerHTML = acumulador;
+});}
 
-document.getElementById ("card-container").innerHTML = acumulador;
 
 function agregarAlCarrito (id) {
     Toastify({
@@ -123,24 +139,24 @@ function agregarAlCarrito (id) {
         position: "right", 
         stopOnFocus: true, 
         style: {
-        background: "linear-gradient(to right, #71777a, #9d9c9a)",
+            background: "linear-gradient(to right, #71777a, #9d9c9a)",
         }, 
         onClick: function(){}
     }).showToast();
-
-const resultado = arrayDeProductos.find ((producto) => producto.id === id);
-
-carrito.push (resultado);
-
-const carritoJSON = JSON.stringify (carrito);
-
-localStorage.setItem ('carrito',carritoJSON);
-
-const totalCarrito = carrito.reduce ((acumulador,producto) => acumulador + producto.precio, 0);
     
-document.getElementById('carrito-elementos').innerHTML = carrito.length + " $ " + totalCarrito;
+    // const resultado = arrayDeProductos.find ((producto) => producto.id === id);
     
-limpiarCarrito.onclick = () =>{limpiar()}
+    carrito.push (resultado);
+    
+    const carritoJSON = JSON.stringify (carrito);
+    
+    localStorage.setItem ('carrito',carritoJSON);
+    
+    const totalCarrito = carrito.reduce ((acumulador,producto) => acumulador + producto.price, 0);
+    
+    document.getElementById('carrito-elementos').innerHTML = carrito.length + " $ " + totalCarrito;
+    
+    limpiarCarrito.onclick = () =>{limpiar()}
     function limpiar() {
         localStorage.clear();
         Swal.fire({
@@ -152,17 +168,19 @@ limpiarCarrito.onclick = () =>{limpiar()}
             confirmButtonText: 'Si, borrar carrito!'
         }).then((result) => {
             if (result.isConfirmed) {
-            Swal.fire(
-                'Eliminado!',
-                'Eliminaste todos los productos del carrito',
-                'success'
-            )
-            document.getElementById('carrito-elementos').innerHTML = carrito.length = 0;
-            }
-        })
-}
-
-botonCarrito.onclick = () => {
-    totalCarrito >= 10000 ? Swal.fire('Actualmente tenes ' + carrito.length + ' productos en el carrito, por un valor de ARS$ ' + (totalCarrito) * 0.9) : Swal.fire('Actualmente tenes ' + carrito.length + ' productos en el carrito, por un valor de ARS$ ' + totalCarrito); 
+                Swal.fire(
+                    'Eliminado!',
+                    'Eliminaste todos los productos del carrito',
+                    'success'
+                    )
+                    document.getElementById('carrito-elementos').innerHTML = carrito.length = 0;
+                }
+            })
+        }
+        
+        botonCarrito.onclick = () => {
+            totalCarrito >= 10000 ? Swal.fire('Actualmente tenes ' + carrito.length + ' productos en el carrito, por un valor de ARS$ ' + (totalCarrito) * 0.9) : Swal.fire('Actualmente tenes ' + carrito.length + ' productos en el carrito, por un valor de ARS$ ' + totalCarrito); 
+        }
     }
-}
+    
+    traerDatos();
